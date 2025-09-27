555b02512d146226af6f2d56787d03df6a24a41a
gilberto <ggmoncada.23@est.ucab.edu.ve>
2025-09-26 21:14:21 -0400

    estructura correcta del proyecto

--- backend/src/main/java/com/software/spring/MessageController.java
+++ backend/src/main/java/com/software/spring/MessageController.java
@@ -1,4 +1,4 @@
-package com.software.spring.blog;
+package com.software.spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
