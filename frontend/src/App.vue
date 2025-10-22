52b7b879c9c49e3899fa3678d3e8373f10d6df2e
Humberto Villacis <humbertovfarma@gmail.com>
2025-10-21 23:16:37 -0400

    Haciendo el home de la aplicacion, eliminando logos que vinieron con la instalacion de vue cli y el componente helloworld

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,179 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <aside>
+    <nav>
+      <h1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="helmet"><title>racing-helmet</title><path d="M2.2,11.2C2,13.6 2.7,15.6 4.2,17.4C5.7,19.2 7.7,20 10.1,20H20.1C20.6,20 21.1,19.8 21.5,19.4C21.9,19 22.1,18.5 22.1,18V17.2C22.1,16.6 22,15.9 21.9,15H13.7C12.7,15 11.9,14.6 11.2,13.9C10.5,13.2 10.1,12.3 10.1,11.4C10.1,9.8 10.8,8.7 12.3,8.1L17.1,6C15.4,4.8 13.4,4.1 11.1,4C8.9,3.8 6.9,4.5 5.1,5.9C3.3,7.3 2.4,9 2.2,11.2M12.1,11.4C12.1,11.8 12.3,12.2 12.6,12.5C12.9,12.8 13.3,13 13.7,13H21.5C20.9,10.8 20,9 18.7,7.6L13.1,9.9C12.4,10.1 12.1,10.6 12.1,11.4Z" /></svg><span>Blog</span></h1>
+
+      <a href="#" class="profile"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>account</title><path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" /></svg><span>Profile</span></a>
+      <a href="#" class="Messages"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>message-reply-outline</title><path d="M20 2H4C2.9 2 2 2.9 2 4V16C2 17.1 2.9 18 4 18H18L22 22V4C22 2.9 21.1 2 20 2M20 17.2L18.8 16H4V4H20V17.2Z" /></svg><span>Chats</span></a>
+      <a href="#" class="createPost"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>briefcase-variant</title><path d="M10 16V15H3L3 19C3 20.11 3.89 21 5 21H19C20.11 21 21 20.11 21 19V15H14V16H10M20 7H16V5L14 3H10L8 5V7H4C2.9 7 2 7.9 2 9V12C2 13.11 2.89 14 4 14H10V12H14V14H20C21.1 14 22 13.1 22 12V9C22 7.9 21.1 7 20 7M14 7H10V5H14V7Z" /></svg><span>Crear Post</span></a>
+      <br>
+      <br>
+      <a href="#" class="getOut"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cog</title><path d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" /></svg><span>Salir</span></a>
+    </nav>
+  </aside>
+  <header class="header"></header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      box-sizing: border-box;
+      text-decoration: none;
+    }
+
+    body{
+      flex-direction: column;
+    }
+
+    aside {
+      flex: 0 0 220px;
+      max-width: 220px;
+      min-width: 180px;
+      height: 100vh;
+      position: sticky;
+      top: 0;
+      background-color: antiquewhite;
+    }
+
+    .header {
+      width: 100%;
+      min-width: 0;
+      box-sizing: border-box;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      height: 10vh;
+      padding: 1rem 2rem;
+      background-color: #f5f5f5;
+      box-shadow: 0 2px 8px rgba(0,0,0,0.07);
+      border-bottom: 1px solid #e0e0e0;
+      position: sticky;
+      top: 0;
+    }
+
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      box-sizing: border-box;
+      text-decoration: none;
+    }
+
+    aside{
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      position: sticky;
+      top: 0;
+      justify-content: flex-start;
+      background-color: antiquewhite;
+      max-width: 20vw;
+      height: 100vh;
+      padding: 1.5rem;
+
+    }
+
+    body{
+      height: 300vh;
+    }
+
+    nav{
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+    }
+
+    h1{
+      display: flex;
+      align-items: center;
+      gap: 0.1em;
+
+      font-family: 'FuenteHeader', sans-serif;
+      font-style: italic;
+
+
+      font-size: 2rem;
+      letter-spacing: -4px;
+      font-weight: 700;
+      margin: 0  0 1.5rem 0;
+      color: black;
+    }
+
+    .helmet{
+      height: 1.5em;
+      width: 1.5em;
+    }
+
+    nav>a{
+      display: flex;
+      align-items: center;
+      gap: 0.6rem;
+      font-family: 'FuenteHeader', sans-serif;
+      font-size: 1.1rem;
+      color: black;
+      margin-bottom: 0.2rem;
+      padding-left: 7px;
+    }
+
+    nav>a:hover{
+      color: blue;
+      cursor: pointer;
+    }
+
+    .header{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 10vh;
+      padding: 1rem 2rem;
+      background-color: #be5858;
+      box-shadow: 0 2px 8px rgba(0,0,0,0.07);
+      border-bottom: 1px solid #e0e0e0;
+      z-index: 10;
+      width: 100%;
+    }
+
+    svg {
+      height: 2em;
+      width: 2em;
+    }
+
 </style>
